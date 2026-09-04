e9d5db1d1edb77deced745da34dc1af3a5719a97
BigBrain82 <brilliantguy14@gmail.com>
2026-09-04 13:40:28 -0400

    Lots of Stuff

---
+++ _BrainClasses/Code/Cards/CupheadSetup.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using ModdingUtils.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class CupheadSetup : MonoBehaviour {
+
+    // Gun Setup
+    public List<CardInfo> cards;
+    Player owner;
+    int random_number;
+
+    // Super Meter Setup
+
+    void Start()
+    {
+        owner = GetComponentInParent<Player>();
+
+        random_number = Random.Range(0, cards.Count);
+
+        ModdingUtils.Utils.Cards.instance.AddCardToPlayer(owner, cards[random_number], false, "", 2f, 2f, true);
+
+    }
+} 
---
+++ _BrainClasses/Code/Controllers/Arrows.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Arrows : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+}
